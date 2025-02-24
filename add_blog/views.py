1e8043f3211daddecb634034a745632220da474c
Alekc Lebdedev <lebedew.alexej2020@gmail.com>
2025-02-24 19:11:20 +0300

    update views for display data

--- add_blog/views.py
+++ add_blog/views.py
@@ -1,3 +1,17 @@
+from django.http import JsonResponse
+from .models import Blog
 from django.shortcuts import render
+import logging
 
-# Create your views here.
+logger = logging.getLogger('add_blog')
+
+def posts(request):
+    if request.path == '/':
+        return JsonResponse({'message': 'Добро пожаловать на страницу!'})
+    elif request.path == '/posts/':
+        data = data_from_database()
+        return JsonResponse({'message': 'Успех', 'data': data})
+
+def data_from_database():
+    data = Blog.objects.all().values('project_title', 'description', 'username')
+    return list(data)
--- project_blog/urls.py
+++ project_blog/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from add_blog.views import posts
+
 
 urlpatterns = [
+    path('', posts),
     path('admin/', admin.site.urls),
+    path('posts/', posts),
 ]
