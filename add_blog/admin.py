61acd05bc0b50f041169f9811b4879ab4b283cf2
Alekc Lebdedev <lebedew.alexej2020@gmail.com>
2025-02-24 17:27:24 +0300

    create new migrations, add gitignore and adding data via admin-panel

--- add_blog/admin.py
+++ add_blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('project_title', 'description', 'username', 'create_post', 'update_post')
+    search_files = ('username', 'project_title')
+    filter_files = ('username', 'create_post', 'update_post')
